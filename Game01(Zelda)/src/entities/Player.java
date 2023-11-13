67f15f190ef575d69892127a6c58a6eb19b5df52
nicolasp <84407787+nickxzin@users.noreply.github.com>
2023-11-13 20:25:30 -0300

    Camera e Clamp

--- Game01(Zelda)/src/entities/Player.java
+++ Game01(Zelda)/src/entities/Player.java
@@ -4,6 +4,8 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 import main.Game;
+import world.Camera;
+import world.World;
 
 public class Player extends Entity{
 
@@ -61,13 +63,16 @@ public class Player extends Entity{
                 }
             }
         }
+
+        Camera.x = Camera.clamp(this.getX() - (Game.WIDTH/2), 0, World.WIDTH*16 - Game.WIDTH);
+        Camera.y = Camera.clamp(this.getY() - (Game.HEIGHT/2), 0, World.HEIGHT*16 - Game.HEIGHT);
     }
 
     public void render (Graphics g){
         if(dir == right_dir){
-            g.drawImage(rightPlayer[index], this.getX(), this.getY(), null);
+            g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
         }else if(dir == left_dir) {
-            g.drawImage(leftPlayer[index], this.getX(), this.getY(), null);
+            g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
         }
     }
     
