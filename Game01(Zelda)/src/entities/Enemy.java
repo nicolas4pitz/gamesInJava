835bbe4c2c4d10bd9c155c32318daedd22636f6a
nicolasp <84407787+nickxzin@users.noreply.github.com>
2023-11-14 19:02:03 -0300

    Sistema de Vida

--- Game01(Zelda)/src/entities/Enemy.java
+++ Game01(Zelda)/src/entities/Enemy.java
@@ -27,18 +27,29 @@ public class Enemy extends Entity{
     }
     
     public void tick(){
-        if ((int)x < Game.player.getX() && World.isFree((int)(x+speed), this.getY()) && !isColliding((int)(x+speed), this.getY())){
-            x += speed;
-        } else if((int)x > Game.player.getX() && World.isFree((int)(x-speed), this.getY()) && !isColliding((int)(x-speed), this.getY())){
-            x -= speed;
-        }
 
-        if((int) y < Game.player.getY() && World.isFree(this.getX(), (int)(y+speed)) && !isColliding(this.getX(), (int)(y+speed))){
-            y += speed;
-        } else if ((int) y > Game.player.getY() && World.isFree(this.getX(), (int)(y-speed)) && !isColliding(this.getX(), (int)(y-speed))){
-            y -= speed;
-        }
+        if (this.isCollidingWithPlayer() == false){
+            if ((int)x < Game.player.getX() && World.isFree((int)(x+speed), this.getY()) && !isColliding((int)(x+speed), this.getY())){
+                x += speed;
+            } else if((int)x > Game.player.getX() && World.isFree((int)(x-speed), this.getY()) && !isColliding((int)(x-speed), this.getY())){
+                x -= speed;
+            }
 
+            if((int) y < Game.player.getY() && World.isFree(this.getX(), (int)(y+speed)) && !isColliding(this.getX(), (int)(y+speed))){
+                y += speed;
+            } else if ((int) y > Game.player.getY() && World.isFree(this.getX(), (int)(y-speed)) && !isColliding(this.getX(), (int)(y-speed))){
+                y -= speed;
+            }
+        } else {
+            //Estamos colidindo
+            if(Game.rand.nextInt(100) < 10){
+                Game.player.life-=Game.rand.nextInt(3);
+                if (Game.player.life <= 0){
+                    System.exit(0);
+                }
+                System.out.println("Vida: "+Game.player.life);
+            }
+        }
         
         frames++;
         if (frames == maxFrames){
@@ -52,9 +63,17 @@ public class Enemy extends Entity{
     
     }
 
+    public boolean isCollidingWithPlayer(){
+        Rectangle enemyCurrent = new Rectangle(this.getX() + maskx, this.getY() + masky, maskw, maskh);
+
+        Rectangle player = new Rectangle(Game.player.getX(), Game.player.getY(), 16, 16);
+        
+        return enemyCurrent.intersects(player);
+    }
+
+
     public boolean isColliding(int xnext, int ynext){
         Rectangle enemyCurrent = new Rectangle(xnext + maskx, ynext + masky, maskw, maskh);
-        
         for (int i = 0; i < Game.enemies.size(); i++){
             Enemy e = Game.enemies.get(i);
             if (e == this){
