372be073ac3c9a73a88a50a026fe23dc7e8745d4
nicolasp <84407787+nickxzin@users.noreply.github.com>
2023-11-14 12:49:14 -0300

    Colisões e IA Enemy

--- Game01(Zelda)/src/entities/Enemy.java
+++ Game01(Zelda)/src/entities/Enemy.java
@@ -2,11 +2,30 @@ package entities;
 
 import java.awt.image.BufferedImage;
 
+import main.Game;
+import world.World;
+
 public class Enemy extends Entity{
+
+    private double speed = 0.2;
 
     public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
         super(x, y, width, height, sprite);
         //TODO Auto-generated constructor stub
     }
     
+    public void tick(){
+        if ((int)x < Game.player.getX() && World.isFree((int)(x+speed), this.getY())){
+            x += speed;
+        } else if((int)x > Game.player.getX() && World.isFree((int)(x-speed), this.getY())){
+            x -= speed;
+        }
+
+        if((int) y < Game.player.getY() && World.isFree(this.getX(), (int)(y+speed))){
+            y += speed;
+        } else if ((int) y > Game.player.getY() && World.isFree(this.getX(), (int)(y-speed))){
+            y -= speed;
+        }
+    }
+
 }
