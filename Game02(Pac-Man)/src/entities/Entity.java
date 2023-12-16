4d0b08787572d5314fc9e74cc20ffaebfaa5e541
nicolasp <84407787+nickxzin@users.noreply.github.com>
2023-12-16 14:12:00 -0300

    Começando o Pac-Man e terminando

--- Game02(Pac-Man)/src/entities/Entity.java
+++ Game02(Pac-Man)/src/entities/Entity.java
@@ -5,47 +5,44 @@ import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 import main.Game;
 import world.Camera;
 import world.Node;
 import world.Vector2i;
+import world.World;
 
 public class Entity {
 
-    public static BufferedImage LIFEPACK_EN = Game.spritesheet.getSprite(48, 32, 16, 16);
-    public static BufferedImage WEAPON_EN = Game.spritesheet.getSprite(0, 48, 16, 16);
-    public static BufferedImage BULLET_EN = Game.spritesheet.getSprite(16, 48, 16, 16);
-    public static BufferedImage ENEMY_EN = Game.spritesheet.getSprite(32, 48, 16, 16);
-    public static BufferedImage ENEMY_FEEDBACK = Game.spritesheet.getSprite(32, 64, 16, 16);
-    public static BufferedImage GUN_RIGHT = Game.spritesheet.getSprite(0, 64, 16, 16);
-    public static BufferedImage GUN_LEFT = Game.spritesheet.getSprite(0, 80, 16, 16);
-
     protected List<Node> path;
+
+    public static BufferedImage Maca_Sprite = Game.spritesheet.getSprite(32, 0, 16, 16);
+    public static BufferedImage Enemy_ROSA = Game.spritesheet.getSprite(32, 32, 16, 16);
+    public static BufferedImage Enemy_AZUL = Game.spritesheet.getSprite(32, 16, 16, 16);
+    public static BufferedImage Enemy_LARANJA = Game.spritesheet.getSprite(48, 32, 16, 16);
+    public static BufferedImage Enemy_VERMELHO = Game.spritesheet.getSprite(48, 16, 16, 16);
 
     public double x;
     public double y;
     protected int z;
     protected int width;
     protected int height;
+    protected double speed;
 
     public int depth;
 
     private BufferedImage sprite;
 
-    public int maskx, masky, mwidth, mheight;
+    public static Random rand = new Random();
 
-    public Entity(int x, int y, int width, int height, BufferedImage sprite) {
+    public Entity(double x, double y, int width, int height, double speed, BufferedImage sprite) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.sprite = sprite;
-
-        this.maskx = 0;
-        this.masky = 0;
-        this.mwidth = width;
-        this.mheight = height;
+        this.speed = speed;
 
     }
 
@@ -61,12 +58,10 @@ public class Entity {
 		}
 		
 	};
-
-    public void setMask(int maskx, int masky, int mwidth, int mheight){
-        this.maskx = maskx;
-        this.masky = masky;
-        this.mwidth = mwidth;
-        this.mheight = mheight;
+    
+    public void uptadeCamera(){
+        Camera.x = Camera.clamp(this.getX() - (Game.WIDTH/2), 0, World.WIDTH*16 - Game.WIDTH);
+        Camera.y = Camera.clamp(this.getY() - (Game.HEIGHT/2), 0, World.HEIGHT*16 - Game.HEIGHT);
     }
 
     public void setX(int newX){
@@ -101,22 +96,6 @@ public class Entity {
 		return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 	}
 
-    public boolean isColliding(int xnext, int ynext){
-        Rectangle enemyCurrent = new Rectangle(xnext + maskx, ynext + masky, mwidth, mheight);
-        for (int i = 0; i < Game.enemies.size(); i++){
-            Enemy e = Game.enemies.get(i);
-            if (e == this){
-                continue;
-            }
-            Rectangle targetEnemy = new Rectangle(e.getX() + maskx, e.getY() + masky, mwidth, mheight);
-            if (enemyCurrent.intersects(targetEnemy)){
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     public void followPath(List<Node> path) {
 		if(path != null) {
 			if(path.size() > 0) {
@@ -144,8 +123,8 @@ public class Entity {
 	}
 
     public static boolean isColliding(Entity e1, Entity e2){
-        Rectangle e1Mask = new Rectangle(e1.getX() + e1.maskx, e1.getY()+e1.masky, e1.mwidth, e1.mheight);
-        Rectangle e2Mask = new Rectangle(e2.getX() + e2.maskx, e2.getY()+e2.masky, e2.mwidth, e2.mheight);
+        Rectangle e1Mask = new Rectangle(e1.getX(), e1.getY(), e1.getWidth(), e1.getHeight());
+        Rectangle e2Mask = new Rectangle(e2.getX(), e2.getY(), e2.getWidth(), e2.getHeight());
 
         return e1Mask.intersects(e2Mask);
     }
