080fa524273eae6f1a256c19604c09d7e9394363
nicolasp <84407787+nickxzin@users.noreply.github.com>
2023-12-19 12:40:02 -0300

    Configuração para o jogo Mario

---
+++ Game04(Mario)/src/graficos/UI.java
@@ -0,0 +1,15 @@
+package graficos;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import main.Game;
+
+public class UI {
+    
+    public void render (Graphics g){
+        
+    }
+
+}
