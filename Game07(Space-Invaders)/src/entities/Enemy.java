30ace49d9ec58a4dd9af38aa8bc3caa2fc1f3e70
nicolasp <84407787+nickxzin@users.noreply.github.com>
2023-12-30 20:08:08 -0300

    Animação do Inimigo

--- Game07(Space-Invaders)/src/entities/Enemy.java
+++ Game07(Space-Invaders)/src/entities/Enemy.java
@@ -13,6 +13,7 @@ import world.World;
 
 public class Enemy extends Entity{
 
+    public int life = 3;
 
     public Enemy(double x, double y, int width, int height, double speed, BufferedImage sprite) {
         super(x, y, width, height, speed, sprite);
@@ -24,6 +25,23 @@ public class Enemy extends Entity{
         if(y >= Game.HEIGHT){
             Game.entities.remove(this);
             return;
+        }
+
+        for(int i = 0; i < Game.entities.size(); i++){
+            Entity e = Game.entities.get(i);
+            if(e instanceof Bullet){
+                if(Entity.isColliding(this, e)){
+                    Game.entities.remove(e);
+                    life--;
+                    if(life == 0){
+                        Explosion explosion = new Explosion(x, y, 16, 16, 0, null);
+                        Game.entities.add(explosion);
+                        Game.entities.remove(this);
+                        return;
+                    }
+                    break;
+                }
+            }
         }
     }
     
